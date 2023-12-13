2e2c604ab48093426941b0dc5ba71c199d9b5bd3
TonosakiRen <ren221603@gmail.com>
2023-12-13 15:38:25 +0900

    MT4_1_4

--- GameScene.cpp
+++ GameScene.cpp
@@ -96,7 +96,21 @@ void GameScene::Update(CommandContext& commandContext){
 	int a = date[1];
 	ImGui::Text("%d", int(a));
 
+	Quaternion rotation = MakeRotateAxisAngleQuaternion(Normalize(Vector3{1.0f,0.4f,-0.2f}),0.45f);
+	Vector3 pointY = { 2.1f,-0.9f,1.3f };
+	Matrix4x4 rotateMatrix = MakeRotateMatrix(rotation);
+	Vector3 rotateByQuaternion = RotateVector(pointY, rotation);
+	Vector3 rotateByMatrix = Transform(pointY, rotateMatrix);
 
+	ImGui::Begin("01-04");
+	ImGui::Text("rotation  %f %f %f %f", rotation.x, rotation.y, rotation.z, rotation.w);
+	ImGui::Text("rotateMatrix  %f %f %f %f", rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2], rotateMatrix.m[0][3]);
+	ImGui::Text("rotateMatrix  %f %f %f %f", rotateMatrix.m[1][0], rotateMatrix.m[1][1], rotateMatrix.m[1][2], rotateMatrix.m[1][3]);
+	ImGui::Text("rotateMatrix  %f %f %f %f", rotateMatrix.m[2][0], rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3]);
+	ImGui::Text("rotateMatrix  %f %f %f %f", rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]);
+	ImGui::Text("rotateByQuaternion  %f %f %f ", rotateByQuaternion.x, rotateByQuaternion.y, rotateByQuaternion.z);
+	ImGui::Text("rotateByMatrix  %f %f %f ", rotateByMatrix.x, rotateByMatrix.y, rotateByMatrix.z);
+	ImGui::End();
 }
 
 void GameScene::TitleInitialize() {
